027143448a60f3f66f399c4cf1a3b8c3de72e122
Ринат Омаров <rinat.omarov@eubank.kz>
2023-12-11 23:55:58 +0600

    базовые типы данных swift

---
+++ MyPlayground.playground/Pages/basicDataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+// Переменные
+var nameOfNeighbor = "Dias"
+nameOfNeighbor
+nameOfNeighbor = "Rinat"
+
+// Базовые типы данных
+//1. String (текстовая строка)
+//2. Int (целое число)
+//3. Float (32-битное число с плавающей точкой)
+//4. Double (64-битное число с плавающей точкой для более длинных переменных)
+//5. Bool (логическое значение «true» или «false»).
+//6. Character
+
+
+//option зажать и узнать какой тип переменная
+// command shift L - swift snippets
+
+//1.Тип String
+//var biggestCountry  = ""
+//let country: String
+//country = "Kazakhstan"
+//biggestCountry = country
+
+//2.Тип Integer
+var country = "Andorra"
+var countryArea = 468
+
+//countryArea = country //str к int не присваивается
+//country = 468 //int к str не присваивается
+
+//Типы Double и Float
+//Тип данных Float способен содержать до 6 чисел после запятой,
+//а тип данных Double способен содержать до 15 чисел.
+let areaOfSmallestCountry = 0.44
+var randomArea: Double = 316
+
+let areaSmallCountry:Float = 0.33
+
+//Тип Boolean
+var isFederalState = false
+
+//Тип Character -  хранить только 1 символ в переменную
+let symbol:Character = "$"
+//let symbol2:Character = "$ " // так нельзя хранить значение - ошибка
